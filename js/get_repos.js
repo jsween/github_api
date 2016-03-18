2cf0472b3722bd50ba14f70e7bc8c2a015634e47
Jon <jonsweeney26@gmail.com>
2016-03-18 10:46:39 -0700

    add avatar to index

--- js/get_repos.js
+++ js/get_repos.js
@@ -3,8 +3,9 @@ var User = require('./../js/user.js').User;
 exports.getRepos = function(apiKey, user_name){
   $.get('https://api.github.com/users/' + user_name + '?access_token=' + apiKey).then(function(response){
     console.log(response);
+    $('#avatar_image').empty();
     var new_user = new User(response);
-    
+    $('#avatar_image').append(new_user.avatar_url);
   }).fail(function(error){
     console.log(error.responseJSON.message);
     return alert(user_name + ' could not be found.');
