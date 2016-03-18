f21e528ebceafb0791a7aab84a2847f70776a216
Jon <jonsweeney26@gmail.com>
2016-03-18 11:05:50 -0700

    add name to results on index

--- js/get_repos.js
+++ js/get_repos.js
@@ -5,7 +5,9 @@ exports.getRepos = function(apiKey, user_name){
     console.log(response);
     $('#avatar_image').empty();
     var new_user = new User(response);
+    console.log(new_user);
     $('#avatar_image').append(new_user.avatar_url);
+    $('#display_name').text(new_user.name);
   }).fail(function(error){
     console.log(error.responseJSON.message);
     return alert(user_name + ' could not be found.');
