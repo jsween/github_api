4307a49de0bf7b01c0a4c7317bbba51a85dba7ae
Jon <jonsweeney26@gmail.com>
2016-03-18 14:00:11 -0700

    add css and more than 30 returned

--- js/get_repos.js
+++ js/get_repos.js
@@ -1,11 +1,12 @@
 exports.getRepos = function(apiKey, user_name){
-  $.get('https://api.github.com/users/' + user_name + '/repos' + '?access_token=' + apiKey).then(function(response){
+  $.get('https://api.github.com/users/' + user_name + '/repos' + '?access_token=' + apiKey + '&sort=created&per_page=100').then(function(response){
     console.log(response);
     $('#repo_links').empty();
-    $('#repo_links').append('<h4>First 30 Repositories</h4>');
+    $('#repo_links').append('<h2>Repositories</h2><hr>');
     for(var i = 0; i < response.length; ++i) {
       console.log("response's length is: " + response.length);
       $('#repo_links').append('<a href="'+ response[i].html_url +'" target="_blank">' + response[i].name + '</a>');
+      $('#repo_links').append(response[i].description + '<hr>');
     }
   }).fail(function(error){
     console.log(error.responseJSON.message);
