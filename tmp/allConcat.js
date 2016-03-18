dedc24f75e3934f2d91c36c5aa96f9b010e980e5
Jon <jonsweeney26@gmail.com>
2016-03-18 09:03:22 -0700

    add get repos function

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -3,13 +3,17 @@
 2.  Get name & avatar
 3.  Get all public repos
 */
-
 var User = require('./../js/user.js').User;
 var moment = require('moment');
 var apiKey = require('./../.env').apiKey;
+var getRepos = require('./../js/get_repos.js').getRepos;
 
-$(document).ready(function(){
+$(function(){
+  $('form#search-user').submit(function(event) {
+    event.preventDefault();
 
 
+  }); //end of submit form
+  getRepos(apiKey);
+}); //end of when ready
 
-  });
