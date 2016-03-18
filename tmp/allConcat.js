8327670080e2f1404750d36cfc4d07f0aff0ba62
Jon <jonsweeney26@gmail.com>
2016-03-18 16:08:32 -0700

    comment out the require moment on browser interface

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,5 +1,5 @@
 var User = require('./../js/user.js').User;
-var moment = require('moment');
+// var moment = require('moment');
 var apiKey = require('./../.env').apiKey;
 var getUserData = require('./../js/get_user_data.js').getUserData;
 var getRepos = require('./../js/get_repos.js').getRepos;
