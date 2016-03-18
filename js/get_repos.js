3704da1f98a1368cb90a272734bd5c3c670e5b7c
Jon <jonsweeney26@gmail.com>
2016-03-18 11:27:54 -0700

    added user properties to index

--- js/get_repos.js
+++ js/get_repos.js
@@ -8,7 +8,9 @@ exports.getRepos = function(apiKey, user_name){
     console.log(new_user);
     $('#avatar_image').append(new_user.avatar_url).append('<hr>');
     $('#display_name').text(new_user.name);
-    $('#display_login').text(new_user.login);
+    $('#display_login').text(new_user.login).append('<hr>');
+    $('#display_num_repos').text(new_user.public_repos).append(' repos');
+    $('#display_repo_url').append('<a href="' + new_user.repos_url + '">View Repositories</a>')
   }).fail(function(error){
     console.log(error.responseJSON.message);
     return alert(user_name + ' could not be found.');
